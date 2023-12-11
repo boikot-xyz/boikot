df337997fb137a0f714dd07a13edcb6df0978ebb
Oscar Saharoy <osaharoy@gmail.com>
2023-12-11 20:08:13 +0000

    site

---
+++ site/app.jsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+    return <h1 className="title">boikot 🐐</h1>;
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
