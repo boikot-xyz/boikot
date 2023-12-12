46092f85ce19e276c63fce4b3cc28dfcf5822887
Oscar Saharoy <osaharoy@gmail.com>
2023-12-12 17:14:14 +0000

    jsnoer

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React from "react";
+import { Jsoner } from "./jsoner.jsx";
 
 export function Home() {
-    return <h1 className="title">boikot 🙅‍♀️ </h1>;
+    return <div>
+        <h1 className="title">boikot 🙅‍♀️ </h1>
+        <Jsoner/>
+    </div>;
 }
 
---
+++ site/src/jsoner.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function Jsoner() {
+    return <div>
+        <textarea/>
+    </div>;
+}
+
