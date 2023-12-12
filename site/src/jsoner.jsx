b64aba71e37382cd275e91713f01bab73aa3592d
Oscar Saharoy <osaharoy@gmail.com>
2023-12-12 17:48:16 +0000

    textarea

--- site/src/jsoner.jsx
+++ site/src/jsoner.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 
+
+const initialState = {
+    comment: "",
+    sources: {},
+};
+
+function tojson(state) {
+    return JSON.stringify(state, null, 2);
+}
+
 export function Jsoner() {
+    const [state, setState] = React.useState(initialState);
+
+    const setComment = e =>
+        setState( oldState => (
+            { ...oldState, comment: e.target.value }
+        ));
+
     return <div>
-        <textarea/>
+        <textarea value={state.comment} onChange={setComment} />
+        <pre>{tojson(state)}</pre>
     </div>;
 }
 
