677a227b8e2ad8cdc9eb499849918ab4ffb0f5b4
Oscar Saharoy <osaharoy@gmail.com>
2023-12-13 20:41:00 +0000

    padding

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Jsoner } from "./jsoner.jsx";
 
+
+
 export function Home() {
-    return <div>
+    return <div style={{ padding: "2rem" }}>
         <h1 className="title">boikot 🙅‍♀️ </h1>
         <Jsoner/>
     </div>;
