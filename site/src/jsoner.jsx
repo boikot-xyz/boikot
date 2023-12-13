d49cadc352f02df7860b03c5a32729fc688313cd
Oscar Saharoy <osaharoy@gmail.com>
2023-12-13 18:51:25 +0000

    start to extract sources

--- site/src/jsoner.jsx
+++ site/src/jsoner.jsx
@@ -10,12 +10,23 @@ function tojson(state) {
     return JSON.stringify(state, null, 2);
 }
 
+function makeSources(comment, oldSources) {
+    const nSources = comment.match(/\[a-z\]/g).length;
+    return {
+        "1": nSources,
+    };
+}
+
 export function Jsoner() {
     const [state, setState] = React.useState(initialState);
 
     const setComment = e =>
         setState( oldState => (
-            { ...oldState, comment: e.target.value }
+            {
+                ...oldState,
+                comment: e.target.value,
+                sources: makeSources(e.target.value, oldState.sources),
+            }
         ));
 
     return <div>
