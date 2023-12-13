b63db72b9a709fd1eaed2ae505a13a1d655e6796
Oscar Saharoy <osaharoy@gmail.com>
2023-12-13 19:26:55 +0000

    rm css

--- site/app.jsx
+++ site/app.jsx
@@ -2,6 +2,5 @@ import * as React from "react"
 import ReactDOM from "react-dom"
 import { createRoot } from "react-dom/client"
 import { Home } from "./src/home.jsx";
-import "./src/app.css";
 
 createRoot(document.querySelector("#root")).render(<Home />);
