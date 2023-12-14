086471fd3056e8a8656acadbec050d6fad16b069
Oscar Saharoy <osaharoy@gmail.com>
2023-12-14 06:34:56 +0000

    title margin

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -4,8 +4,8 @@ import { Jsoner } from "./jsoner.jsx";
 
 
 export function Home() {
-    return <div style={{ padding: "2rem" }}>
-        <h1 className="title">boikot 🙅‍♀️ </h1>
+    return <div style={{ padding: "2rem", maxWidth: "50rem" }}>
+        <h1 style={{ marginBottom: "1rem" }}>boikot 🙅‍♀️ </h1>
         <Jsoner/>
     </div>;
 }
