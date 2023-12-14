cb6a6dbfb94e38f72e7d3c9c0ca9a0cda9db96b5
Oscar Saharoy <osaharoy@gmail.com>
2023-12-14 22:19:24 +0000

    blog rendering

--- site/src/blog/blog.jsx
+++ site/src/blog/blog.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import blog1 from "./1.txt";
 
 export function Blog() {
     return <div>
         <h1> blog </h1>
+        { blog1 }
     </div>;
 }
