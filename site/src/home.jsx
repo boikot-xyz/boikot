3c56ded96e62ee01d20c73d17122dfb5deb47552
Oscar Saharoy <osaharoy@gmail.com>
2023-12-14 06:52:11 +0000

    start blog

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { tojson, Jsoner } from "./jsoner.jsx";
+import { Blog } from "./blog/blog.jsx";
 import boikot from "../../boikot.json";
 
 
@@ -8,6 +9,7 @@ export function Home() {
     return <div style={{ padding: "2rem", maxWidth: "50rem" }}>
         <h1 style={{ marginBottom: "1rem" }}>boikot 🙅‍♀️ </h1>
         <Jsoner/>
+        <Blog />
         <pre style={{ whiteSpace: "pre-wrap" }}>
             { tojson(boikot) }
         </pre>
