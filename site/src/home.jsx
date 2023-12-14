331cf456cffa10d553b10a5a32b8f5fd7f2848b2
Oscar Saharoy <osaharoy@gmail.com>
2023-12-14 18:07:35 +0000

    stack component

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -1,18 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
 import { tojson, Jsoner } from "./jsoner.jsx";
 import { Blog } from "./blog/blog.jsx";
 import boikot from "../../boikot.json";
 
+export const Stack = styled.div`
+    display: grid;
+    gap: 1rem;
+`;
 
 
 export function Home() {
-    return <div style={{ padding: "2rem", maxWidth: "50rem" }}>
+    return <Stack style={{ padding: "2rem", maxWidth: "50rem" }}>
         <h1 style={{ marginBottom: "1rem" }}>boikot 🙅‍♀️ </h1>
         <Jsoner/>
         <Blog />
         <pre style={{ whiteSpace: "pre-wrap" }}>
             { tojson(boikot) }
         </pre>
-    </div>;
+    </Stack>;
 }
 
