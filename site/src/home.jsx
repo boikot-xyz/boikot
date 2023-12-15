ed454a93b453bc46eee1873441545139419f6aee
Oscar Saharoy <osaharoy@gmail.com>
2023-12-15 23:34:45 +0000

    center UI

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -6,15 +6,22 @@ import { Blog } from "./blog/blog.jsx";
 import { Stack, WrappingPre } from "./components.jsx";
 import boikot from "../../boikot.json";
 
+const Centerer = styled.div`
+    display: grid;
+    place-items: center;
+    width: 100vw;
+`;
 
 export function Home() {
-    return <Stack style={{ padding: "2rem", maxWidth: "50rem" }}>
-        <h1 style={{ marginBottom: "1rem" }}>boikot 🙅‍♀️ </h1>
-        <Jsoner/>
-        <Blog />
-        <WrappingPre>
-            { JSON.stringify(boikot, null, 2) }
-        </WrappingPre>
-    </Stack>;
+    return <Centerer>
+        <Stack style={{ padding: "2rem", maxWidth: "50rem" }}>
+            <h1 style={{ marginBottom: "1rem" }}>boikot 🙅‍♀️ </h1>
+            <Jsoner/>
+            <Blog />
+            <WrappingPre>
+                { JSON.stringify(boikot, null, 2) }
+            </WrappingPre>
+        </Stack>
+    </Centerer>
 }
 
