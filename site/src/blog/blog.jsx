569b0e79a0babe38200a47bef049dabad30e5998
Oscar Saharoy <osaharoy@gmail.com>
2023-12-15 22:35:35 +0000

    blog rendering loop

--- site/src/blog/blog.jsx
+++ site/src/blog/blog.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+
 import blog1 from "./1.txt";
+
+const blogs = [
+    blog1,
+];
 
 export function Blog() {
     return <div>
         <h1> blog </h1>
-        { blog1 }
+        { blogs.map( blog =>
+            <pre>{ blog }</pre> ) }
     </div>;
 }
