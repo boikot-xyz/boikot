9243a6616cd98a865a0b911d1d413e59bbfb8c73
Oscar Saharoy <osaharoy@gmail.com>
2023-12-16 16:09:37 +0000

    expand getLogo.js

--- scripts/getLogo.js
+++ scripts/getLogo.js
@@ -2,26 +2,68 @@
 
 const { JSDOM } = require("jsdom");
 
-// document.querySelector(".infobox-image.logo img").src.match(/.+?\.svg/)[0].replace("thumb/", "");
 
-async function getLogo(url) {
+const searchEndpoint = searchQuery =>
+    `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=description%7Cinfo&inprop=url&generator=prefixsearch&redirects=&gpssearch=${searchQuery}&gpsnamespace=0&gpslimit=1`;
 
-    if( !url ) return console.log("No url provided!");
 
-    const page = await (await fetch(url)).text();
-    const dom = new JSDOM(page);
+async function getWikipediaPage( searchQuery ) {
 
+    const response = await (await fetch(
+        searchEndpoint(searchQuery))
+    ).json();
+    const page = response.query.pages[0];
+
+    return page;
+}
+
+function getLogoUrl( pageDOM ) {
+
+    const document = pageDOM.window.document;
     const logoImg =
-        dom.window.document.querySelector(".infobox-image.logo img") ??
-        dom.window.document.querySelector(".infobox-image img");
+        document.querySelector(".infobox-image.logo img") ??
+        document.querySelector(".infobox-image img");
 
     const logoUrl = logoImg.src
         .match(/.+?\.svg/)[0]
         .replace("thumb/", "")
         .replace("//upload", "https://upload");
 
-    console.log( logoUrl );
+    return logoUrl;
 }
 
-getLogo( process.argv[2] );
+function getSiteUrl( pageDOM ) {
+
+    const document = pageDOM.window.document;
+    const infoBoxLabels = [...document.querySelectorAll(
+        "table.infobox.vcard tr th.infobox-label"
+    )];
+    const siteLabel = infoBoxLabels.filter( 
+        el => el.innerHTML.includes("Website")
+    )[0];
+    const siteUrl = siteLabel.parentElement.querySelector("a").href;
+
+    return siteUrl;
+}
+
+async function getInfo( searchQuery ) {
+
+    const page = await getWikipediaPage( searchQuery );
+    const pageHTML = await (await fetch(page.fullurl)).text();
+    const pageDOM = new JSDOM( pageHTML );
+
+    return {
+        searchQuery: searchQuery,
+        logoUrl: getLogoUrl( pageDOM ),
+        siteUrl: getSiteUrl( pageDOM ),
+        wikipediaUrl: page.fullurl,
+        wikipediaTitle: page.title,
+        wikipediaDescription: page.description,
+    };
+}
+
+( async () => {
+    const info = await getInfo( process.argv[2] );
+    console.log( info );
+} )();
 
