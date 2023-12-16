95a340e0fe5e155415347f3cb1ca635922aff0f2
Oscar Saharoy <osaharoy@gmail.com>
2023-12-16 01:29:47 +0000

    getLogo script

---
+++ scripts/getLogo.js
@@ -0,0 +1,22 @@
+const { JSDOM } = require("jsdom");
+
+// document.querySelector(".infobox-image.logo img").src.match(/.+?\.svg/)[0].replace("thumb/", "");
+
+async function getLogo(url) {
+    const page = await (await fetch(url)).text();
+    const dom = new JSDOM(page);
+
+    const logoImg =
+        dom.window.document.querySelector(".infobox-image.logo img") ??
+        dom.window.document.querySelector(".infobox-image img");
+
+    const logoUrl = logoImg.src
+        .match(/.+?\.svg/)[0]
+        .replace("thumb/", "")
+        .replace("//upload", "https://upload");
+
+    console.log( logoUrl );
+}
+
+getLogo( process.argv[2] );
+
