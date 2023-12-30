7f8b69fe3eeee2044e5cacffcec563aa158e019e
Oscar Saharoy <osaharoy@gmail.com>
2023-12-30 19:28:20 +0000

    remove companies from homepage

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 import { Jsoner } from "./jsoner.jsx";
-import { Companies } from "./companies.jsx";
 import { Centerer, Header, Stack, WrappingPre } from "./components.jsx";
 import boikot from "../../boikot.json";
 
@@ -10,10 +9,6 @@ export function Home() {
     return <Centerer>
         <Header />
         <Jsoner/>
-        <Companies/>
-        <WrappingPre>
-            { JSON.stringify(boikot, null, 4) }
-        </WrappingPre>
     </Centerer>
 }
 
