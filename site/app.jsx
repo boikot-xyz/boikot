e886e668af88dd8ffd34bd6b23f8bc925bb25012
Oscar Saharoy <osaharoy@gmail.com>
2023-12-30 16:06:07 +0000

    only load eruda if wanted

--- site/app.jsx
+++ site/app.jsx
@@ -5,3 +5,11 @@ import { Router } from "./src/router.jsx";
 
 createRoot(document.querySelector("#react-entrypoint"))
     .render(<Router />);
+
+if( window.location.hostname.includes("localhost") ) {
+    const script = document.createElement('script');
+    script.src="https://cdn.jsdelivr.net/npm/eruda";
+    document.body.append(script);
+    script.onload = () => eruda.init();
+}
+
