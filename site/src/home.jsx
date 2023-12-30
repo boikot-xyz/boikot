f0c2d8f659a10887c161d070a815c50663b3a088
Oscar Saharoy <osaharoy@gmail.com>
2023-12-30 21:23:09 +0000

    improve homepage copy

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -10,11 +10,11 @@ export function Home() {
         <Header />
         <h2> boikot is a community-led initiative to make
             data on company ethics transparent and accessible. </h2>
-        <p> We are building a transparent, community-curated, freely 
-            accessible collection of corporate ethics records. By 
-            documenting ethical and unethical business practices, 
-            we aim to inform consumer choice, raise the cost of 
-            harmful business decisions, and incentivize companies to act 
+        <p> We are building a community-curated, transparent, freely
+            accessible collection of corporate ethics records. By
+            documenting ethical and unethical business practices,
+            we aim to inform consumer choice, raise the cost of
+            harmful business decisions, and incentivise companies to act
             responsibly in the public interest. </p>
         <Link to="/companies"> view company records </Link>
         <Link to="/companies/edit"> get involved by adding a new company </Link>
