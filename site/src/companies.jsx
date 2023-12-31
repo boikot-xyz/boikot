4b4a618aef92691f9f3e461d2d9b89c264ec1173
Oscar Saharoy <osaharoy@gmail.com>
2023-12-31 17:14:21 +0000

    safari link wrapping fix

--- site/src/companies.jsx
+++ site/src/companies.jsx
@@ -29,8 +29,9 @@ export function Company({entry}) {
         </p> }
         { !!Object.keys(entry.sources).length && <h3> Sources </h3> }
         { Object.entries(entry.sources).map( ([ key, url ]) =>
-            <p key={key}>
+            <p key={key} style={{ maxWidth: "46rem" }}>
                 [{key}] <a href={url} style={{
+                    overflowWrap: "break-word",
                     overflowWrap: "anywhere",
                 }}>
                     {url}
