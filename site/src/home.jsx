5888abaf8b475fa1d04ed9fcd71ee6d01beafba0
Oscar Saharoy <osaharoy@gmail.com>
2024-01-09 21:21:31 +0000

    worst offenders

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import { Page, Stack } from "./components.jsx";
+import { CompanyHeader } from "./companies.jsx";
 import boikot from "../../boikot.json";
 
 export function Home() {
@@ -18,6 +19,12 @@ export function Home() {
                 responsibly in the public interest. </p>
             <Link to="/companies"> view company records </Link>
             <Link to="/companies/edit"> get involved by adding a new company </Link>
+            <h1> worst offenders </h1>
+            { Object.values(boikot.companies)
+                .filter( entry => !!entry.comment )
+                .toSorted( (a,b) => a.score - b.score )
+                .slice(0, 10)
+                .map( entry => <CompanyHeader entry={ entry } link /> ) }
         </Stack>
     </Page>
 }
