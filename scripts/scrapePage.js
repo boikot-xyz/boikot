e650cf0c6253c0f0348d63376caaaa5dbdd32f76
Oscar Saharoy <osaharoy@gmail.com>
2024-01-14 15:27:01 +0000

    improving scrapePage.js

--- scripts/scrapePage.js
+++ scripts/scrapePage.js
@@ -6,16 +6,17 @@ const { JSDOM } = require("jsdom");
 // "https://uk.finance.yahoo.com/news/barclays-share-price-plummeted-6-161400453.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvLnVrLw&guce_referrer_sig=AQAAAND0Vxno_XUzjaC0MXOV4h6hBfpsUAP-aB7qKao9JudPxCEGxzeIVtSJw1HlNHBc7SbtJKtOigH5GtyIFlKGMRvOpJK4nA6ZQm0LOE960JO5Pcah2POYOM2aq4aLXyHolOKUbpdLPm2ANRm8aj_83f9zqpc-LsksEiL1xQujOpJ6"
 // "https://news.sky.com/story/more-attacks-in-red-sea-if-uk-didnt-act-says-david-cameron-as-he-defends-military-action-13048021"
 
-async function scrape( url, main=true ) {
+async function scrape( url ) {
 
     const pageHTML = await (await fetch(url)).text();
     const pageDOM = new JSDOM( pageHTML );
     const document = pageDOM.window.document;
-    const paragraphs = [...document.querySelectorAll(`${main ? "main " : ""}p`)];
+
+    let paragraphs = [...document.querySelectorAll("main p")];
+    if( !paragraphs.length )
+        paragraphs = [...document.querySelectorAll("p")];
+
     const text = paragraphs.map(p => p.textContent.trim()).join(" ");
-
-    if( main && !text ) return await scrape(url, false);
-
     return { text };
 }
 
