27bfc0540cf5c80d29f0107f3686bf6fc69bceb1
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2024-02-11 16:25:07 +0000

    improving assemble script

--- scripts/assemble.mjs
+++ scripts/assemble.mjs
@@ -1,11 +1,28 @@
 #!/usr/bin/env node
 
-const readline = require('readline');
-const { JSDOM } = require("jsdom");
+import  readline from "readline";
+import { JSDOM } from "jsdom";
+import boikot from "../boikot.json" assert { type: "json" };
 
 
 const DEFAULT_OLLAMA_ENDPOINT = "http://localhost:11434/api/generate";
-
+const fetchOptions = {
+    headers: {
+        "Host": "www.houstonchronicle.com",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-GB,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
+    },
+};
 
 async function getInput() {
     const rl = readline.createInterface({
@@ -34,13 +51,17 @@ async function getUrls() {
 
 async function scrape( url ) {
 
-    const pageHTML = await (await fetch(url)).text();
+    const pageHTML = await (await fetch(url, fetchOptions)).text();
     const pageDOM = new JSDOM( pageHTML );
     const document = pageDOM.window.document;
 
     let paragraphs = [...document.querySelectorAll("main p")];
     if( !paragraphs.length )
         paragraphs = [...document.querySelectorAll("p")];
+    if( !paragraphs.length ) {
+        console.log("could not get content from", url, "- please paste it here:");
+        return await getInput();
+    }
 
     const text = paragraphs.map(p => p.textContent.trim()).join(" ");
     return text;
@@ -62,22 +83,24 @@ async function askLlama( ollamaEndpoint, prompt ) {
 }
 
 const summaryPrompt = text =>
-    `Here is some text:\n\n${text}\n\nPlease summarise that text in one succinct sentence.`;
+    `Here is some text scraped from a webpage:\n\n${text}\n\nPlease summarise the content of the webpage in one succinct sentence.`;
 
 async function summarise( ollamaEndpoint, url ) {
     const text = await scrape(url);
     const summary = await askLlama( ollamaEndpoint, summaryPrompt(text) );
+    console.log({ url, text, summary });
     return { url, summary, text };
 }
 
 const commentPrompt = text =>
-    `Here is some text:\n\n${text}\n\nPlease summarise that text in two sentences, making sure to keep the reference markers (eg. [1]).`;
+    `Here are some examples of two-sentence company summaries:\n\n${boikot.companies.apple.comment}\n\n${boikot.companies.bbc.comment}\n\n${boikot.companies.sony.comment}\n\nHere is some text:\n\n${text}\n\nPlease summarise that text into a two-sentence company summary like those above, including a few words on all the key points. After you include information from a given section, make sure to include its refernce marter eg. [1], [2], [3]...`;
 
 async function getComment( ollamaEndpoint, summaries ) {
     const text = summaries.map(
         (summary, i) => `${summary.summary.replace(/\.$/, '')} [${i+1}].`
     ).join(" ");
     const comment = await askLlama( ollamaEndpoint, commentPrompt(text) );
+    console.log( "\n\n=====\n\n", commentPrompt(text), "\n\n=====\n\n" );
     return comment;
 }
 
@@ -95,11 +118,11 @@ async function main() {
         (res, url, i) => ({ ...res, [i+1]: url }), {}
     );
 
-    const summaries = await Promise.all( urls.map(
-        url => summarise( ollamaEndpoint, url )
-    ) );
-    const comment = await getComment( ollamaEndpoint, summaries );
+    const summaries = [];
+    for( const url of urls )
+        summaries.push( await summarise( ollamaEndpoint, url ) );
 
+    const comment = await getComment( ollamaEndpoint, summaries );
     const output = { comment, sources };
     console.log(output);
 }
