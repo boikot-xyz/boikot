bbcb91b735b77742c02ee3247583871e59a28493
Oscar Saharoy <osaharoy@gmail.com>
2024-02-12 20:52:34 +0000

    reorganise homepage

--- site/src/home.jsx
+++ site/src/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-import { Page, PillButton, Stack } from "./components.jsx";
+import { FlexRow, Page, PillButton, Stack } from "./components.jsx";
 import { CompanyHeader, Score } from "./companies.jsx";
 import boikot from "../../boikot.json";
 
@@ -17,29 +17,31 @@ export function Home() {
                 we aim to inform consumer choice, raise the cost of
                 harmful business decisions, and incentivise companies to
                 act responsibly in the public interest. </p>
-            <h2> our ratings </h2>
-            <p style={{ lineHeight: "1.4rem" }}>
-                We rank companies on a scale from <Score score="0" />{" "}
-                to <Score score="100"/>, based on their positive and
-                negative actions, with <Score score="50" /> being a
-                neutral score.  If you disagree with a company score,
-                please submit an updated record for it! </p>
             <h1 style={{ marginTop: "1rem" }}> worst offenders 💩 </h1>
             { Object.values(boikot.companies)
                 .filter( entry => !!entry.comment )
                 .toSorted( (a,b) => a.score - b.score )
                 .slice(0, 10)
                 .map( entry => <CompanyHeader entry={ entry } link /> ) }
-            <Link to="/companies">
-                <PillButton $outline>
-                    🧐 View all company records
-                </PillButton>
-            </Link>
-            <Link to="/companies/edit">
-                <PillButton $outline>
-                    🤝 Get involved by adding a new company
-                </PillButton>
-            </Link>
+            <FlexRow>
+                <Link to="/companies">
+                    <PillButton $outline>
+                        🧐 View all company records
+                    </PillButton>
+                </Link>
+                <Link to="/companies/edit">
+                    <PillButton $outline>
+                        🤝 Get involved by adding a new company
+                    </PillButton>
+                </Link>
+            </FlexRow>
+            <h2 style={{ marginTop: "1rem" }}> our ratings ⚖️  </h2>
+            <p style={{ lineHeight: "1.4rem" }}>
+                We rank companies on a scale from <Score score="0" />{" "}
+                to <Score score="100"/>, based on their positive and
+                negative actions, with <Score score="50" /> being a
+                neutral score.  If you disagree with a company score,
+                please submit an updated record for it! </p>
         </Stack>
     </Page>
 }
