9d9e75ef8735dd1ff1bdeea19567a5b976764af3
Oscar Saharoy <osaharoy@gmail.com>
2024-02-13 01:21:56 +0000

    statement score mvp

--- site/src/statement-score.jsx
+++ site/src/statement-score.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Page, PillButton, Stack } from "./components.jsx";
+import { getKey, Page, PillButton, Stack } from "./components.jsx";
+import { Company } from "./companies.jsx";
+import boikot from "../../boikot.json";
+
 
 async function getDataURL( file ) {
     const fileReader = new FileReader();
@@ -9,6 +12,49 @@ async function getDataURL( file ) {
         fileReader.onload = e => resolve(e.target.result);
         fileReader.readAsDataURL(file);
     } );
+}
+
+async function getPageText( pdfDocument, pageNumber ) {
+    const page = await pdfDocument.getPage( pageNumber );
+    const textContent = await page.getTextContent();
+    return textContent.items.map(
+        t => t.hasEOL ? t.str + "\n" : t.str
+    ).join("");
+}
+
+async function getResults( fileDataURL, setResults ) {
+    if (typeof pdfjsLib === "undefined")
+        alert("Could not access PDF reading utility :(");
+    pdfjsLib.GlobalWorkerOptions.workerSrc =
+      "https://unpkg.com/pdfjs-dist@4.0.379/build/pdf.worker.min.mjs";
+
+    const pdfDocument = await pdfjsLib.getDocument({ url: fileDataURL }).promise;
+    const pageNumbers = [...Array(pdfDocument.numPages).keys()].map(x => x+1);
+    const pdfText = ( await Promise.all(
+        pageNumbers.map( pageNumber => getPageText( pdfDocument, pageNumber ) )
+    ) ).join("\n").toLowerCase();
+    setResults({
+        companies: Object.values(boikot.companies)
+            .filter( entry =>
+                pdfText.includes(entry.names[0].toLowerCase())
+            )
+            .map(getKey),
+    });
+}
+
+const resultsCardStyle = {
+};
+
+function Results({ results }) {
+    if( !results ) return null;
+    return <Stack style={ resultsCardStyle }>
+        <h2> 📊 results </h2>
+        <p> we found { results.companies.length } companies in your
+            statement which you can read more about below. </p>
+        { results.companies.map( key =>
+            <Company entry={ boikot.companies[key] } />
+        ) }
+    </Stack>;
 }
 
 function PDFPreview({ src }) {
@@ -21,6 +67,8 @@ function PDFPreview({ src }) {
 
 export function StatementScore() {
     const [ fileDataURL, setFileDataURL ] = React.useState("");
+    const [ results, setResults ] = React.useState(null);
+
     return <Page>
         <Stack>
             <h1> Statement Score </h1>
@@ -31,12 +79,14 @@ export function StatementScore() {
                 entirely on your own device and will not be sent out via the
                 internet whatsoever. However you can also use a sample bank
                 statement if you would prefer not to use your own! </p>
+            <Results results={results} />
             <h2> upload a bank statement </h2>
             <input type="file" accept=".pdf,application/pdf" onChange={
                 async e => setFileDataURL( await getDataURL(e.target.files[0]) )
             } />
             <PDFPreview src={fileDataURL} /> 
-            { fileDataURL && <PillButton>
+            { fileDataURL && <PillButton
+                onClick={() => getResults(fileDataURL, setResults)}>
                 💥 click to score your statement!
             </PillButton> }
             { !fileDataURL && <PillButton>
