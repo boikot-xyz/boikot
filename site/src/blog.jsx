31e2750a0cb65a7dcd67cf24a97ecc29012cef36
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2024-02-18 17:10:51 +0000

    split showdown into its own script

--- site/src/blog.jsx
+++ site/src/blog.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { useLoaderData } from "react-router-dom";
-import showdown from 'showdown';
 
 import { Stack, Page, WrappingPre } from "./components.jsx";
 
