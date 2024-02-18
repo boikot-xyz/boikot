fa33e57eb64d73b079f9dd166929208bb68dac6e
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2024-02-18 15:20:07 +0000

    render blogs with showdown

--- site/src/blog.jsx
+++ site/src/blog.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useLoaderData } from "react-router-dom";
+import showdown from 'showdown';
 
 import { Stack, Page, WrappingPre } from "./components.jsx";
 
@@ -24,15 +25,36 @@ export async function loadBlogs() {
     return blogs;
 }
 
+function BlogHeader({ blog }) {
+    const title = blog.match(/# (.+)/)?.[1];
+    const author = blog.match(/## Author: (.+)/)?.[1];
+    const date = blog.match(/## Date: (.+)/)?.[1];
+    const text = blog.match(/\n\n(.+)/ms)?.[1].trim();
+    const converter = new showdown.Converter();
+    const html = converter.makeHtml(text);
+
+    return <Stack gap="0.5rem">
+        <h2> {title} </h2>
+        <h4> {date} </h4>
+        <h4> by {author} </h4>
+        <p style={{ marginTop: "0.5rem", whiteSpace: "pre-wrap" }}
+            dangerouslySetInnerHTML={{__html: html}} />
+    </Stack>;
+}
+
 export function Blogs() {
     const blogs = useLoaderData();
     return <Page>
         <Stack>
             <h1> blog </h1>
+            <p> Welcome to the boikot blog! We post here about companies, ethics
+                and the development of the website. </p>
             { blogs.map( blog =>
-                <WrappingPre key={blog.slice(0,10)}>
-                    { blog }
-                </WrappingPre> ) }
+                <>
+                    <hr />
+                    <BlogHeader blog={blog} />
+                </>
+            ) }
         </Stack>
     </Page>;
 }
