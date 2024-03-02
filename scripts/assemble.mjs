5f5c395bdab93081fcb27450fd803eab22649492
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2024-03-02 20:12:57 +0000

    assemble becoming scary

--- scripts/assemble.mjs
+++ scripts/assemble.mjs
@@ -1,43 +1,19 @@
 #!/usr/bin/env node
 
-import  readline from "readline";
+import readline from "readline";
 import { JSDOM } from "jsdom";
+import slugify from "slugify";
 import boikot from "../boikot.json" assert { type: "json" };
 
-
-const DEFAULT_OLLAMA_ENDPOINT = "http://localhost:11434/api/generate";
-const fetchOptions = {
-    headers: {
-        "Host": "www.houstonchronicle.com",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Language": "en-GB,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Pragma": "no-cache",
-        "Cache-Control": "no-cache",
-    },
-};
-
-async function askLlama( ollamaEndpoint, prompt ) {
-    const ollamaResponse = await fetch( ollamaEndpoint, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            model: "gemma",
-            stream: false,
-            prompt: prompt,
-        })
-    });
-    return ( await ollamaResponse.json() ).response;
+function unique( array ) {
+    return array.filter( (item, index) => array.indexOf(item) === index );
 }
+
+export const getKey = name =>
+    slugify(name).toLowerCase();
+
+
+// === IO ===
 
 async function getInput() {
     const rl = readline.createInterface({
@@ -64,6 +40,99 @@ async function getUrls() {
     return urls;
 }
 
+
+// === llm interface ===
+
+const DEFAULT_OLLAMA_ENDPOINT = "http://localhost:11434/api/generate";
+
+async function askLlama( prompt, ollamaEndpoint=DEFAULT_OLLAMA_ENDPOINT ) {
+    const ollamaResponse = await fetch( ollamaEndpoint, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "openhermes",
+            stream: false,
+            prompt: prompt,
+        })
+    });
+    return ( await ollamaResponse.json() ).response;
+}
+
+const summaryPrompt = text =>
+    `Here is some text scraped from a webpage:\n\n${text}\n\nPlease succinctly summarise the content of the webpage.`;
+
+async function summarise( url ) {
+    const text = await scrape(url);
+    const summary = await askLlama( summaryPrompt(text) );
+    console.log(url, "summary:", summary );
+    return { url, summary, text };
+}
+
+const commentPrompt = text =>
+    `Here are some examples of two-sentence company summaries:\n\n${boikot.companies.apple.comment}\n\n${boikot.companies.bbc.comment}\n\n${boikot.companies.sony.comment}\n\nHere is some text:\n\n${text}\n\nPlease summarise that text into a two-sentence company summary like those above, including a few words on all the key points. After you include information from a given section, make sure to include its refernce marter eg. [1], [2] & [3]. Respond with the two-sentence summary only. Your summary:`;
+
+async function getComment( summaries ) {
+    const text = summaries.map(
+        (summary, i) => `${summary.summary.replace(/\.$/, '')} [${i+1}].`
+    ).join(" ");
+    const comment = await askLlama( commentPrompt(text) );
+    // console.log( "\n\n===== assemble prompt =====\n\n", commentPrompt(text), "\n\n==========\n\n" );
+    return comment;
+}
+
+async function getCompanyName( comment ) {
+    return askLlama( `Here is some company information: "${comment}". Please respond with the name of the company only and no other text. The company name is:` );
+}
+
+async function getNames( companyName ) {
+    const namesString = ( await askLlama( 
+        `Please respond with a list of 7 commonly used names for the ${companyName} company, in the format: [ "name1", "name2", ... ]`
+    ) ).replaceAll(/\n/g, "");
+    if( !namesString.match( /^\s*\[\s*(".+")+\s*\]\s*$/ ) ) return [ companyName ];
+
+    try {
+        const names = eval( namesString );
+        return unique([ companyName, ...names ]);
+    } catch {
+        return [ companyName ];
+    }
+}
+
+async function getTags( companyName ) {
+    const tagsString = ( await askLlama( 
+        `Please respond with a list of 7 tags, not necessariliy from the list above, that could apply to the ${companyName} company, in the format: [ "tag1", "tag2", ... ]`
+    ) ).replaceAll(/\n/g, "");
+    if( !tagsString.match( /^\s*\[\s*(("|').+("|'),?)+\s*\]\s*$/ ) ) return [];
+
+    try {
+        return eval( tagsString );
+    } catch {
+        return [];
+    }
+}
+
+// === web scraping ===
+
+const fetchOptions = {
+    headers: {
+        "Host": "www.houstonchronicle.com",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:122.0) Gecko/20100101 Firefox/122.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-GB,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache",
+    },
+};
+
 async function scrape( url ) {
 
     const pageHTML = await (await fetch(url, fetchOptions)).text();
@@ -82,33 +151,6 @@ async function scrape( url ) {
     return text;
 }
 
-const summaryPrompt = text =>
-    `Here is some text scraped from a webpage:\n\n${text}\n\nPlease succinctly summarise the content of the webpage.`;
-
-async function summarise( ollamaEndpoint, url ) {
-    const text = await scrape(url);
-    const summary = await askLlama( ollamaEndpoint, summaryPrompt(text) );
-    console.log(url, "summary:", summary );
-    return { url, summary, text };
-}
-
-const commentPrompt = text =>
-    `Here are some examples of two-sentence company summaries:\n\n${boikot.companies.apple.comment}\n\n${boikot.companies.bbc.comment}\n\n${boikot.companies.sony.comment}\n\nHere is some text:\n\n${text}\n\nPlease summarise that text into a two-sentence company summary like those above, including a few words on all the key points. After you include information from a given section, make sure to include its refernce marter eg. [1], [2], [3]...`;
-
-async function getComment( ollamaEndpoint, summaries ) {
-    const text = summaries.map(
-        (summary, i) => `${summary.summary.replace(/\.$/, '')} [${i+1}].`
-    ).join(" ");
-    const comment = await askLlama( ollamaEndpoint, commentPrompt(text) );
-    // console.log( "\n\n===== assemble prompt =====\n\n", commentPrompt(text), "\n\n==========\n\n" );
-    return comment;
-}
-
-async function getCompanyName( ollamaEndpoint, comment ) {
-    return askLlama( ollamaEndpoint, `Here is some company information: "${comment}". Please respond with the name of the company only. The company name is: ` );
-}
-
-
 const searchEndpoint = searchQuery =>
     `https://en.wikipedia.org/w/api.php?action=query&format=json&formatversion=2&prop=description%7Cinfo&inprop=url&generator=prefixsearch&redirects=&gpssearch=${searchQuery}&gpsnamespace=0&gpslimit=2`;
 
@@ -123,6 +165,7 @@ async function getWikipediaPage( searchQuery ) {
     ).json();
     // Log search results
     // console.log(JSON.stringify(response, null, 4));
+    if( !response.query ) return null;
     const page = response.query.pages
         .filter(isNotDisambiguation)[0];
 
@@ -159,34 +202,47 @@ function getSiteUrl( pageDOM ) {
     return siteUrl;
 }
 
+function getTickers( pageDOM ) {
+
+    const document = pageDOM.window.document;
+    const infoBoxLabels = [...document.querySelectorAll(
+        "table.infobox.vcard tr th.infobox-label"
+    )];
+    const tradedLabel = infoBoxLabels.filter( 
+        el => el.innerHTML.includes("Traded") 
+    )[0];
+    if( !tradedLabel ) return [];
+
+    const tradedStrings = [...tradedLabel.parentElement.querySelectorAll("li")]
+        .map(el => el.textContent)
+
+    return unique( 
+        tradedStrings
+        .map( s => s.match( /[ a-z]+:\s([a-z]+)/i ) )
+        .filter( match => match )
+        .map( match => match[1] )
+    );
+}
+
 async function getInfo( searchQuery, showExtraInfo = false ) {
 
     const page = await getWikipediaPage( searchQuery );
+    if( !page ) return { tickers: [] };
+
     const pageHTML = await (await fetch(page.fullurl)).text();
     const pageDOM = new JSDOM( pageHTML );
-
-    const extraInfo = {
-        searchQuery: searchQuery,
-        wikipediaUrl: page.fullurl,
-        wikipediaTitle: page.title,
-        wikipediaDescription: page.description,
-    };
 
     return {
         logoUrl: getLogoUrl( pageDOM ),
         siteUrl: getSiteUrl( pageDOM ),
+        tickers: getTickers( pageDOM ),
     };
 }
 
 
+
 async function main() {
     console.log("boikot assemble 🧩\nenter urls:\n");
-
-    console.log(
-        `enter ollama endpoint [ blank for ${DEFAULT_OLLAMA_ENDPOINT} ]:`
-    );
-    const ollamaEndpoint = await getInput() || DEFAULT_OLLAMA_ENDPOINT;
-    console.log(`using ${ollamaEndpoint} as ollama endpoint\n`);
 
     const urls = await getUrls();
     const sources = urls.reduce(
@@ -195,20 +251,34 @@ async function main() {
 
     const summaries = [];
     for( const url of urls )
-        summaries.push( await summarise( ollamaEndpoint, url ) );
+        summaries.push( await summarise( url ) );
 
-    const comment = await getComment( ollamaEndpoint, summaries );
+    const comment = await getComment( summaries );
 
-    console.log( "comment:", comment );
-
-    const companyName = await getCompanyName( ollamaEndpoint, comment );
+    const companyName = await getCompanyName( comment );
 
     console.log( "companyName:", companyName );
 
-    const { logoUrl, siteUrl } = await getInfo( companyName );
-    const output = { names: [ companyName ], comment, sources, logoUrl, siteUrl };
+    const { logoUrl, siteUrl, tickers } = await getInfo( companyName );
+    const names = await getNames( companyName );
+    const tags = await getTags( companyName );
 
-    console.log(JSON.stringify(output, null, 4));
+    console.log( "\n\n", comment, "\n\nscore?" );
+    const scoreString = await getInput();
+
+    const output = {
+        names: [ ...names, ...tickers ],
+        comment,
+        sources,
+        tags,
+        score: parseInt(scoreString),
+        ownedBy: [],
+        logoUrl,
+        siteUrl,
+        updatedAt: (new Date()).toISOString(),
+    };
+
+    console.log( "\n\n", `"${ getKey(names[0]) }":`, JSON.stringify(output, null, 4), "\n\n" );
 }
 
 main();
