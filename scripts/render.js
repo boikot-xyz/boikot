6c6d83eea16e79bf54946eec23ed543cd1247cd6
Oscar Saharoy <osaharoy@gmail.com>
2024-03-29 09:23:00 +0000

    render.js

---
+++ scripts/render.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+const { JSDOM, VirtualConsole } = require("jsdom");
+const fs = require('fs').promises;
+const path = require('path');
+
+const virtualConsole = new VirtualConsole();
+virtualConsole.on("error", () => {});
+virtualConsole.on("warn", () => {});
+virtualConsole.on("info", () => {});
+virtualConsole.on("dir", () => {});
+
+const visitedUrls = {};
+
+async function scrape( url ) {
+    
+    if( visitedUrls[url.pathname] ) return;
+    visitedUrls[url.pathname] = true;
+    const pageHTML = await (await fetch(url)).text();
+    const scripts = pageHTML.match(
+        /<script.+?<\/script>/gs
+    ).join("\n");
+    const pageDOM = new JSDOM(
+        pageHTML, {
+            runScripts: "dangerously",
+            resources: "usable",
+            url: url,
+            referrer: url,
+            contentType: "text/html",
+            includeNodeLocations: true,
+            storageQuota: 10000000,
+            virtualConsole,
+        }
+    );
+    return new Promise( resolve =>
+        pageDOM.window.renderCallback = () =>
+            resolve(pageDOM
+                .serialize()
+                .replace("</body>", scripts + "</body>")
+            )
+    );
+}
+
+async function renderTree( url, targetDir=null ) {
+
+    let filePath = (targetDir || url.host)
+        + url.pathname.replace( /\/?(index.html)?$/, "/index.html" ); 
+
+    console.log( "\n::: Rendering", url.toString(), "to", filePath );
+
+    let text;
+    try {
+        text = await scrape( url );
+        if( !text ) return;
+    } catch(error) {
+        console.error(error);
+        return;
+    }
+
+    await fs.mkdir( path.dirname(filePath), { recursive: true } );
+    await fs.writeFile( filePath, text );
+
+    const origin = ( new URL(url) ).origin;
+    const urls = 
+        [...text.matchAll(/<a.+?href=\"([^\"]+)\".+?<\/a>/g)]
+            .map( match => match[1] )
+            .map( path => new URL(path, origin) )
+            .filter( otherUrl => otherUrl.origin === origin )
+            .map( otherUrl =>
+                new URL(otherUrl.pathname, otherUrl.origin)
+            );
+    for( const otherUrl of urls )
+        await renderTree( otherUrl, targetDir );
+}
+
+
+( async () => {
+    await renderTree( new URL(process.argv[2]), process.argv[3] );
+    console.log( "\n Finished rendering", process.argv[2] );
+} )();
+
