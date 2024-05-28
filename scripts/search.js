6050efd0a9b4c179e0c3344f9bb51b4172fd2983
Oscar Saharoy <osaharoy@gmail.com>
2024-05-28 19:41:29 +0100

    start on search engine

---
+++ scripts/search.js
@@ -0,0 +1,6 @@
+/*
+curl "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=microsoft&fq=scandal&api-key=x" | jq ".response.docs[] | .web_url"
+
+curl -X GET "https://archive.org/wayback/available?url=http://tc.eserver.org/"
+*/
+
