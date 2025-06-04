5393f9e50ebc373ca60fca8138277f61fb1b0a55
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2025-06-04 10:55:19 +0100

    vitest

---
+++ scripts/wiki.test.js
@@ -0,0 +1,118 @@
+import { describe, it, expect } from "vitest";
+import _ from "lodash";
+import { getWikipediaInfo, getWikipediaPage } from "./wiki.js";
+
+const targetWikipediaPages = [
+  ["apple", "https://en.wikipedia.org/wiki/Apple_Inc."],
+  ["samsung", "https://en.wikipedia.org/wiki/Samsung"],
+  ["bp", "https://en.wikipedia.org/wiki/BP"],
+  ["channel 4", "https://en.wikipedia.org/wiki/Channel_4"],
+  ["byd", "https://en.wikipedia.org/wiki/BYD_Company"],
+  ["moonpig", "https://en.wikipedia.org/wiki/Moonpig"],
+  ["dell", "https://en.wikipedia.org/wiki/Dell"],
+  ["tfl", "https://en.wikipedia.org/wiki/Transport_for_London"],
+  ["ikea", "https://en.wikipedia.org/wiki/IKEA"],
+  ["abercrombie and fitch", "https://en.wikipedia.org/wiki/Abercrombie_%26_Fitch"],
+  ["anker", "https://en.wikipedia.org/wiki/Anker_Innovations"],
+  ["b&q", "https://en.wikipedia.org/wiki/B%26Q"],
+  ["a24", "https://en.wikipedia.org/wiki/A24"],
+  ["minute maid", "https://en.wikipedia.org/wiki/Minute_Maid"],
+  ["colman's", "https://en.wikipedia.org/wiki/Colman%27s"],
+];
+
+describe("getWikipediaPage", () => {
+  targetWikipediaPages.forEach(([companyName, expectedUrl]) => {
+    it(`returns correct Wikipedia URL for ${companyName}`, async () => {
+      const url = await getWikipediaPage(companyName);
+      expect(url).toBe(expectedUrl);
+    });
+  });
+});
+
+const targetWikipediaInfos = [
+  ["apple", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Apple_Inc.",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg",
+    siteUrl: "https://www.apple.com/",
+  }],
+  ["samsung", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Samsung",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b7/Samsung_Black_icon.svg",
+    siteUrl: "https://www.samsung.com/",
+  }],
+  ["bp", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/BP",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/en/d/d2/BP_Helios_logo.svg",
+    siteUrl: "https://bp.com/",
+  }],
+  ["channel 4", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Channel_4",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/en/9/9b/Channel_4_%28On_Demand%29_2023.svg",
+    siteUrl: "http://www.channel4.com/",
+  }],
+  ["byd", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/BYD_Company",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/9/99/BYD_Company%2C_Ltd._-_Logo.svg",
+    siteUrl: "https://www.bydglobal.com/",
+  }],
+  ["moonpig", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Moonpig",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/en/a/aa/Moonpig_Logo.svg",
+    siteUrl: "http://moonpig.com/",
+  }],
+  ["dell", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Dell",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/1/18/Dell_logo_2016.svg",
+    siteUrl: "https://www.dell.com/",
+  }],
+  ["tfl", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Transport_for_London",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/3/3e/TfL_roundel_%28no_text%29.svg",
+    siteUrl: "https://tfl.gov.uk/",
+  }],
+  ["ikea", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/IKEA",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Ikea_logo.svg",
+    siteUrl: "https://about.ikea.com/",
+  }],
+  ["abercrombie and fitch", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Abercrombie_%26_Fitch",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/3/3e/Abercrombie_%26_Fitch_logo.svg",
+    siteUrl: "http://abercrombie.com/",
+  }],
+  ["anker", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Anker_Innovations",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a4/Anker_Innovations_Co._Ltd._Logo.png",
+    siteUrl: "https://anker.com/",
+  }],
+  ["b&q", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/B%26Q",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c0/B%26Q_company_logo.svg",
+    siteUrl: "https://diy.com/",
+  }],
+  ["a24", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/A24",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b7/A24_logo.svg",
+    siteUrl: "http://a24films.com/",
+  }],
+  ["minute maid", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Minute_Maid",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b3/Minute_Maid_2023.svg",
+    siteUrl: "http://minutemaid.com/",
+  }],
+  ["colman's", {
+    wikipediaUrl: "https://en.wikipedia.org/wiki/Colman%27s",
+    logoUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5b/Buckinghamshire_Railway_Colmans_advert.jpg",
+    siteUrl: undefined,
+  }],
+];
+
+describe("getWikipediaInfo", () => {
+  targetWikipediaInfos.forEach(([companyName, expected]) => {
+    it(`returns correct info for ${companyName}`, async () => {
+      const result = await getWikipediaInfo(companyName);
+      expect(result).toEqual(expected);
+    });
+  });
+});
+
