3c47d68d943147c9dd96333f3ef302db563c1105
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2025-06-26 14:43:20 +0100

    rustscrape working ok

--- scripts/rustscrape/src/main.rs
+++ scripts/rustscrape/src/main.rs
@@ -1,26 +1,20 @@
 use std::error::Error;
-use headless_chrome::Browser;
-use headless_chrome::LaunchOptions;
-use headless_chrome::protocol::cdp::Page;
-use url::{Url, Host, Position};
 use std::env;
-use std::fs;
-use std::iter::FusedIterator;
-
-use ego_tree::iter::{Edge, Traverse};
-use scraper::{Html, Selector, Node};
-use scraper::element_ref::Text;
-
+use headless_chrome::{Browser, LaunchOptions};
+use url::Url;
 
 fn main() -> Result<(), Box<dyn Error>> {
+
+    let arg = env::args().nth(1).expect("please provid URL as a command line arg");
+    let url = Url::parse(&arg).expect("please provid URL as a command line arg");
+
     let browser_options = LaunchOptions {
         headless: true,
         ..Default::default()
     };
     let browser = Browser::new(browser_options)?;
     let user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36";
-    let url = Url::parse("https://stackoverflow.com/questions/79646974/can-you-help-me-fix-those-errors-plz")?;
-    let origin = format!("{}://{}/", url.scheme(), url.host_str().unwrap());
+    let origin = format!("{}://{}/", url.scheme(), url.host_str().expect("please provid URL as a command line arg"));
 
     let mut tab = browser.new_tab()?;
     tab.set_user_agent(user_agent, None, None);
@@ -30,40 +24,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     tab.set_user_agent(user_agent, None, None);
 
     tab.navigate_to(url.as_str())?;
-    tab.wait_for_element("p")?;
 
-    let html_content = tab.get_content()?;
-    let main_p_selector = Selector::parse("main p").unwrap();
-    let p_selector = Selector::parse("p").unwrap();
-    let body_selector = Selector::parse("body").unwrap();
-
-    let document = Html::parse_document(&html_content);
-
-    let result: String = document
-        .select(&body_selector)
-        .next()
-        .unwrap()
-        .traverse()
-        .filter_map(|edge| {
-            if let Edge::Open(node) = edge {
-                if let Some(text) = node.value().as_text() {
-                    if let Some(parent_element) = node.parent().unwrap().value().as_element() {
-                        if parent_element.name() == "script" 
-                            || parent_element.name() == "style" 
-                            || parent_element.name() == "noscript" {
-                            return None;
-                        }
-                        return Some(text.text.to_string());
-                    }
-                }
-            }
-            None
-        })
-        .filter(|text| !text.trim().is_empty())
-        .collect::<Vec<String>>()
-        .join("");
-
-    println!("{}", result);
+    if let Ok(main) = tab.wait_for_element("main") {
+        println!("{}", main.get_inner_text()?); 
+    }
+    else if let Ok(body) = tab.wait_for_element("body") {
+        println!("{}", body.get_inner_text()?); 
+    }
+    else {
+        println!("Could not get main or body element from page to read text from")
+    }
 
     Ok(())
 }
