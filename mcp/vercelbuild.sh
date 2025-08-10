42e52cec03cd6dc5e2eda7efb33b2bd14c911978
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2025-08-10 09:19:19 +0100

    add boikot.json to mcp server files

--- mcp/vercelbuild.sh
+++ mcp/vercelbuild.sh
@@ -4,5 +4,10 @@ set -x
 
 mkdir public
 echo "boikot MCP server served from /api/mcp" > public/index.html
-curl "https://raw.githubusercontent.com/boikot-xyz/boikot/refs/heads/main/boikot.json" > boikot.json
+
+if [ -f ../boikot.json ]; then
+    cp ../boikot.json .
+else
+    curl "https://raw.githubusercontent.com/boikot-xyz/boikot/refs/heads/main/boikot.json" > boikot.json
+fi
 
