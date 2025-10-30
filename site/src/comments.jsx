9e67de23ce0a100ce6866a22798bde9fc571e64c
Oscar Saharoy <osaharoy@gmail.com>
2025-10-30 22:55:22 +0000

    finish comments

--- site/src/comments.jsx
+++ site/src/comments.jsx
@@ -4,33 +4,7 @@ import { getKey, formatDateString, Badge, Card, FlexRow, Icon, IconButton, Page,
 import publicComments from "../../comments.json";
 
 
-const localComments = {
-    "companies": {
-        "barclays": {
-            "comments": [
-                {
-                    "username": "oscar",
-                    "commentText": "I have heard of barclays putting predatory fines on people in overdraft too 😠 taking advantage of them when they are stuggling",
-                    "createdAt": "2025-10-30T12:09:30.315Z"
-                },
-                {
-                    "username": "oscar",
-                    "commentText": "I have heard of barclays putting predatory fines on people in overdraft too 😠 taking advantage of them when they are stuggling",
-                    "createdAt": "2025-10-31T12:09:30.315Z"
-                }
-            ]
-        },
-        "hsbc": {
-            "comments": [
-                {
-                    "username": "oscar",
-                    "commentText": "I have heard of hsbc putting predatory fines on people in overdraft too 😠 taking advantage of them when they are stuggling",
-                    "createdAt": "2025-10-30T12:09:30.315Z"
-                }
-            ]
-        }
-    }
-};
+const localComments = JSON.parse(window.localStorage.getItem("localComments") || '{ "companies": {} }');
 const allComments = publicComments;
 
 for( const [ key, entry ] of Object.entries(localComments.companies) ) {
@@ -47,27 +21,40 @@ for( const [ key, entry ] of Object.entries(localComments.companies) ) {
             ))
     );
 }
-implement local saving on submit
+
+
+function saveComment( event, entry ) {
+    const commentData = Object.fromEntries(new FormData(event.target));
+
+    if( !localComments.companies[getKey(entry)] )
+        localComments.companies[getKey(entry)] = { comments: [] };
+    localComments.companies[getKey(entry)].comments = [...localComments.companies[getKey(entry)].comments, commentData];
+
+    window.localStorage.setItem("localComments", JSON.stringify(localComments));
+}
 
 
 function NewComment({ entry, setAddingComment }) {
     const [ username, setUsername ] = React.useState("anonymous");
-    const [ commentText, setCommentText ] = React.useState("anonymous");
+    const [ commentText, setCommentText ] = React.useState("");
     const createdAt = (new Date()).toISOString();
 
-    return <form name="new-comment" method="post">
+    return <form
+        name="new-comment" method="post"
+        onSubmit={ event => saveComment( event, entry ) + setAddingComment(false) }>
         <Stack gap="0.5rem" style={{ margin: "0.5rem 0" }}>
             <input type="hidden" name="form-name" value="new-comment" />
             <input type="hidden" name="createdAt" value={ createdAt } />
-            <Row style={{ width: "100%", justifySelf: "stretch", gridTemplateColumns: "max-content 1fr", gap: "0.5rem" }}>
-                <p> Username: </p>
+            <Row style={{ width: "100%", justifySelf: "stretch", gridTemplateColumns: "max-content 1fr", gap: "0.5rem", background: "#fff1", paddingLeft: "0.75rem", borderRadius: "0.5rem" }}>
+                <p style={{ opacity: 0.5 }}> Username: </p>
                 <input
                     autoFocus name="username" placeholder="Enter your name"
                     value={username} onChange={e => setUsername(e.target.value)}
-                    style={{ border: "none", background: "#fff1", fontSize: "1rem", minWidth: 0 }} />
+                    style={{ border: "none", background: "transparent", fontSize: "1rem", minWidth: 0, paddingLeft: "0.5rem" }} />
             </Row>
             <textarea
                 name="commentText" placeholder="Enter your comment"
+                    value={commentText} onChange={e => setCommentText(e.target.value)}
                 style={{ border: "none", height: "6rem", background: "#fff1", fontWeight: 200 }} />
             <p style={{ fontSize: "0.8rem", textAlign: "justify" }}>
                 ℹ️  Only the username and comment text you provide above will be stored!
@@ -75,7 +62,7 @@ function NewComment({ entry, setAddingComment }) {
                 and comment, and make these publicly accessible. </p>
             <FlexRow style={{ justifySelf: "end", marginTop: "0.5rem" }}>
                 <PillButton $outline onClick={e => e.preventDefault() + setAddingComment(false)}>❌  Cancel</PillButton>
-                <PillButton type="submit">✅  Submit</PillButton>
+                <PillButton type="submit" disabled={ !commentText }>✅  Submit</PillButton>
             </FlexRow>
         </Stack>
     </form>;
@@ -103,7 +90,7 @@ export function Comments({ entry }) {
         <Row style={{ width: "100%", justifySelf: "stretch", gridTemplateColumns: "max-content 1fr max-content" }}>
             <p> {entryComments.length} Comment{entryComments.length === 1 ? "" : "s"} </p>
             <hr />
-            <PillButton $outline $small onClick={ () => setAddingComment(!addingComment) }>💭  Add a Comment!</PillButton>
+            <PillButton $outline onClick={ () => setAddingComment(!addingComment) }>💭  Add a Comment!</PillButton>
         </Row>
         { addingComment && <NewComment entry={entry} setAddingComment={setAddingComment} /> }
         { entryComments.map( entryComment => <ExistingComment comment={entryComment} /> ) }
