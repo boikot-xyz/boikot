049a142dcff4ea225a4c5bf334bc6b62810a3fdd
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2026-03-12 20:05:16 +0000

    loccitane

--- backend/src/brands.js
+++ backend/src/brands.js
@@ -5,37 +5,15 @@ import slugify from "slugify";
 import _ from "lodash";
 
 const html = `
-<li><a href="https://en.wikipedia.org/wiki/Amana_Corporation" title="Amana Corporation">Amana</a></li>
-<li><span class="noprint" style="font-size:85%; font-style: normal;"> [<a href="https://pt.wikipedia.org/wiki/Brastemp" class="extiw" title="pt:Brastemp">Brastemp</a>]</span> (Brazil)</li>
-<li>Challenger (Colombia)</li>
-<li>Consul (Brazil)</li>
-<li>Diqua (China)</li>
-<li>Estate</li>
-<li>EveryDrop</li>
-<li>Gladiator GarageWorks</li>
-<li>Hefei Sanyo (China)<sup id="cite_ref-Rohit_31-1" class="reference"><a href="https://en.wikipedia.org/wiki/Whirlpool_Corporation#cite_note-Rohit-31"><span class="cite-bracket">[</span>31<span class="cite-bracket">]</span></a></sup></li>
-<li><a href="https://en.wikipedia.org/wiki/John_Inglis_and_Company" title="John Inglis and Company">Inglis</a> (Canada and US)</li>
-<li><a href="https://en.wikipedia.org/wiki/InSinkErator" title="InSinkErator">InSinkErator</a></li>
-<li><a href="https://en.wikipedia.org/wiki/Jenn-Air" class="mw-redirect" title="Jenn-Air">Jenn-Air</a></li>
-<li><a href="https://en.wikipedia.org/wiki/KitchenAid" title="KitchenAid">KitchenAid</a></li>
-<li><span class="noprint" style="font-size:85%; font-style: normal;"> [<a href="https://fr.wikipedia.org/wiki/Laden_(marque)" class="extiw" title="fr:Laden (marque)">Laden</a>]</span> (France)</li>
-<li><a href="https://en.wikipedia.org/wiki/Maytag" title="Maytag">Maytag</a></li>
-<li>Polar</li>
-<li>Roper</li>
-<li><a href="https://en.wikipedia.org/wiki/Royalstar" title="Royalstar">Royalstar</a> (China)</li>
-<li><a href="https://en.wikipedia.org/wiki/Indesit" title="Indset">Indesit</a></li>
-<li><a href="https://en.wikipedia.org/wiki/Hotpoint" title="Hotpoint">Hotpoint</a></li>
-<li>Stinol</li>
-<li>True Fresh</li>
-<li>Whirlpool</li>
-<li><a href="https://en.wikipedia.org/wiki/Yummly" title="Yummly">Yummly</a></li></body></html>`;
+<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"></head><body><a href="https://en.wikipedia.org/wiki/Melvita" title="">Melvita</a></body></html>
+<html><head><meta http-equiv="content-type" content="text/html; charset=utf-8"></head><body><a href="https://en.wikipedia.org/wiki/Sol_de_Janeiro" title="Sol de Janeiro">Sol de Janeiro</a></body></html>
+`;
 
 const tags = [
-    "retail",
-    "electronics"
+    "consumer goods"
 ];
-const score = 8;
-const ownedBy = "whirlpool";
+const score = 30;
+const ownedBy = "l'occitane";
 
 const pages = _.uniqBy(
     [...html.matchAll(/<a.+?href="(.+?)".*?>(.+?)<\/a>/g)]
